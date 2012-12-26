c816ac8b33b3a25be1663a3a0d7f4dfba90693c0
Bulain <bulain@gmail.com>
2012-12-26 11:32:44 +0800

    update axis2 demo for timeout and jks

--- src/test/java/com/bulain/axis2/service/DemoServiceClient.java
+++ src/test/java/com/bulain/axis2/service/DemoServiceClient.java
@@ -6,8 +6,12 @@ import static org.junit.Assert.assertNotNull;
 import java.io.IOException;
 import java.rmi.RemoteException;
 
+import org.apache.axis2.client.Options;
+import org.apache.axis2.client.Stub;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
+import org.apache.axis2.transport.http.HTTPConstants;
+import org.apache.axis2.transport.http.HttpTransportProperties;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -39,6 +43,29 @@ public class DemoServiceClient {
     @Before
     public void setUp() throws IOException {
         stub = new DemoServiceStub(ctx, "http://localhost:8082/axis2-demo/services/DemoService");
+        //setHttpBasicAuthHeader(stub, "user01", "password01");
+        //setTimeoutHeader(stub);
+    }
+
+    void setHttpBasicAuthHeader(Stub stub, String username, String password) {
+        Options options = stub._getServiceClient().getOptions();
+        HttpTransportProperties.Authenticator auth = new HttpTransportProperties.Authenticator();
+        auth.setUsername(username);
+        auth.setPassword(password);
+        auth.setPreemptiveAuthentication(true);
+        options.setProperty(HTTPConstants.AUTHENTICATE, auth);
+        stub._getServiceClient().setOptions(options);
+    }
+    
+    void setTimeoutHeader(Stub stub){
+        Options options = stub._getServiceClient().getOptions();
+        //1
+        options.setProperty(HTTPConstants.SO_TIMEOUT, new Integer(120000));
+        options.setProperty(HTTPConstants.CONNECTION_TIMEOUT, new Integer(120000));
+        //2
+        //options.setTimeOutInMilliSeconds(120000);
+        
+        stub._getServiceClient().setOptions(options);
     }
 
     @Test
